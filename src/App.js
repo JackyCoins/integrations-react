a65ee013704c6438122369a683defa1223d6f01d
Nikita Rusanov <rusanov.n.d@gmail.com>
2023-01-04 19:38:21 +0700

    Added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
-import {ContentfulPage} from "./pages/ContentfulPage";
+import "./App.css";
+import { ContentfulPage } from "./pages/ContentfulPage";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const ROUTES = {
+  root: "/",
+  contentful: "/contentful",
+  typeform: "/typeform",
+  hubspot: "/hubspot",
+  tilda: "/tilda",
+  builder: "/builder",
+};
 
 function App() {
   return (
-    <div className="App">
-      <ContentfulPage />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to={ROUTES.root}>Home</Link>
+            </li>
+            <li>
+              <Link to={ROUTES.contentful}>Contentful</Link>
+            </li>
+            <li>
+              <Link to={ROUTES.typeform}>typeform</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path={ROUTES.root} element={<ContentfulPage />} />
+          <Route path={ROUTES.contentful} element={<ContentfulPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
