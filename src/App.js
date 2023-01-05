dc70d725e0c658e735ab5ca538fd1859cf2ded30
Nikita Rusanov <rusanov.n.d@gmail.com>
2023-01-05 20:20:24 +0700

    Added ROUTES

--- src/App.js
+++ src/App.js
@@ -1,41 +1,72 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 import { ContentfulPage } from "./pages/ContentfulPage";
 import { TypeformPage } from "./pages/TypeformPage";
 
 const ROUTES = {
-  root: "/",
-  contentful: "/contentful",
-  typeform: "/typeform",
-  hubspot: "/hubspot",
-  tilda: "/tilda",
-  builder: "/builder",
+  root: {
+    url: "/",
+    title: "Home",
+  },
+  contentful: {
+    url: "/contentful",
+    title: "Contentful",
+  },
+  typeform: {
+    url: "/typeform",
+    title: "Typeform",
+  },
+  hubspot: {
+    url: "/hubspot",
+    title: "Hubspot",
+  },
+  tilda: {
+    url: "/tilda",
+    title: "Tilda",
+  },
+  builder: {
+    url: "/builder",
+    title: "Builder",
+  },
 };
 
 function App() {
+  const tabs = Object.entries(ROUTES).map(([key, { title, url }]) => {
+    return (
+      <li>
+        <NavLink
+          key={key}
+          to={url}
+          className={({ isActive }) =>
+            isActive ? "text-blue-800" : "text-blue-500"
+          }
+        >
+          {title}
+        </NavLink>
+      </li>
+    );
+  });
+
   return (
     <Router>
       <div className="flex flex-col h-full">
-        <nav>
-          <ul className={"grid grid-flow-col"}>
-            <li>
-              <Link to={ROUTES.root}>Home</Link>
-            </li>
-            <li>
-              <Link to={ROUTES.contentful}>Contentful</Link>
-            </li>
-            <li>
-              <Link to={ROUTES.typeform}>Typeform</Link>
-            </li>
-          </ul>
+        <nav className={"p-2 shadow-2xl"}>
+          <ul className={"grid grid-flow-col justify-items-center"}>{tabs}</ul>
         </nav>
 
-        <Routes>
-          <Route path={ROUTES.root} element={<ContentfulPage />} />
-          <Route path={ROUTES.contentful} element={<ContentfulPage />} />
-          <Route path={ROUTES.typeform} element={<TypeformPage />} />
-        </Routes>
+        <div className={"p-6 flex flex-grow bg-gray-100"}>
+          <Routes>
+            <Route path={ROUTES.root.url} element={<ContentfulPage />} />
+            <Route path={ROUTES.contentful.url} element={<ContentfulPage />} />
+            <Route path={ROUTES.typeform.url} element={<TypeformPage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
