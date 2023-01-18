104e99fbb46043f4aaa4f36b86ba740c43824c99
Nikita Rusanov <rusanov.n.d@gmail.com>
2023-01-18 20:45:48 +0700

    Added onQuestionChanged

--- src/pages/TypeformPage.jsx
+++ src/pages/TypeformPage.jsx
@@ -3,7 +3,14 @@ import { Widget } from "@typeform/embed-react";
 export const TypeformPage = () => {
   return (
     <div className={"flex flex-grow"}>
-      <Widget id="iUesUyX7" style={{ width: "100%" }} className="my-form" />
+      <Widget
+        id="iUesUyX7"
+        style={{ width: "100%" }}
+        className="my-form"
+        onQuestionChanged={() => {
+          alert("onQuestionChanged");
+        }}
+      />
     </div>
   );
 };
